f4308c008b77910ee1da56d2ec9f776280b678ef
azudi <jerryazubuike001@gmail.com>
2022-11-09 16:41:45 +0100

    say

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import "./assets/styles/main.scss"
-import RootRoutes from './navigation';
+import RootRoutes from './navigation/IndexRoutes';
 
 
 function App() {
